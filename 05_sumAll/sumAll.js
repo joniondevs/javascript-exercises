a47e6737a280dbfb1913a395a67d07352576a84d
jonion <joniondev@gmail.com>
2024-04-17 16:23:12 +0300

    fix: sum all on ex 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(typeof a == "number" && a >= 0 && typeof b == "number" && b >= 0){
+        let min = a <= b ? a : b;
+        let max = a > b ? a : b;
 
+        for(let i = min; i <= max; i++){
+            sum += i;
+        }
+        
+        return sum;
+    }
+
+    return "ERROR";
 };
 
 // Do not edit below this line
