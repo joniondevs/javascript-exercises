a4e8f82a4496856620c0bf04bb672adf19266747
jonion <joniondev@gmail.com>
2024-04-17 16:28:42 +0300

    fix: calculate leap year on ex 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(typeof year == "number")
+    {
+        if(year % 100 == 0){
+            return year % 400 == 0;
+        }
 
+        return year % 4 == 0;
+    }
 };
 
 // Do not edit below this line
