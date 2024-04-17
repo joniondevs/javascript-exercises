cfa81db1675a29a68aee24ec857a9ce66807e7a5
jonion <joniondev@gmail.com>
2024-04-17 16:47:44 +0300

    fix: temparature conversions on ex 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  if(typeof degrees == "number"){
+    let celcius = (degrees -32) * (5/9);
+
+    return Math.round((celcius * 10)) / 10;
+  }
+
+  return "ERROR"
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  if(typeof degrees == "number")
+  {
+    let fahrenheit = degrees * (9/5) + 32;
+    return Math.round(fahrenheit * 10) / 10; 
+  }
+
+  return "ERROR"
 };
 
 // Do not edit below this line
