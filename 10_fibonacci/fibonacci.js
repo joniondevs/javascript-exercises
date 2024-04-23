58118067ecd79d7823cd25413e3d2ed1bcf42b1c
jonion <joniondev@gmail.com>
2024-04-23 17:49:20 +0300

    Feat logic to get nth member of fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const phi = (1 + Math.sqrt(5)) / 2;
+const fibonacci = function(member) {
+    
+    let n = parseInt(member);
+    if(typeof n == "number" && n >= 0)
+    {
+        return Math.round((Math.pow(phi, n) - Math.pow(-phi, -n)) / Math.sqrt(5));
+    }
 
+    return "OOPS";
 };
 
 // Do not edit below this line
