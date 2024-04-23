ad7527f7336f96f6224ea06be14461fcdc180427
jonion <joniondev@gmail.com>
2024-04-23 18:19:47 +0300

    Feat find oldest person from people.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => getAge(a.yearOfBirth, a.yearOfDeath) - getAge(b.yearOfBirth, b.yearOfDeath));
+    return people[people.length-1];
 };
+
+function getAge(birthYear, passedYear)
+{
+    if(typeof birthYear == "undefined"){ birthYear = new Date().getFullYear(); }
+    if(typeof passedYear == "undefined"){ passedYear = new Date().getFullYear(); }
+
+    return parseInt(passedYear) - parseInt(birthYear);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
