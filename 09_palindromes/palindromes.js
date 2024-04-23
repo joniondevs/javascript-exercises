702fd5bced31f7358f7526bf54215a1f152afdc3
jonion <joniondev@gmail.com>
2024-04-23 17:13:54 +0300

    Feat check if give string is a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let punctuation =/[\.\s+,?!:;]/g;
+    
+    // clean string not to have any punctuation or spaces.
+    str = str.toLowerCase()
+    .replace(punctuation,"")
+    
+    let letters = str.split("");
+    let reversed = letters.reverse().join("");
 
+    return str === reversed;
 };
 
 // Do not edit below this line
